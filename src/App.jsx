a4f5d21afae460d1eae77666baf3bb5d31744c4b
jodijonatan <jdi102350@gmail.com>
2025-03-13 17:03:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function Square() {
+  return <button className="square">X</button>;
+}
+
+function App() {
+  return (
+    <div className="board">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
+export default App;
